c758fb78b704d8547f91605ccd463b295802fb57
soniaakter09 <sa0308355@gmail.com>
2024-02-20 16:05:57 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let phonePrice = 20000;
+let myBudget = 10000;
+
+if(myBudget <= phonePrice){
+    console.log(' phone kibo');
+}else if(myBudget >= phonePrice){
+    console.log(' phone kinte parbo na')
+}else{
+    console.log('Amr budger er sathe na mille ami phone kinte parbo na')
+}
---
+++ sonia.js
@@ -0,0 +1,8 @@
+let Color = 'red';
+let borka = 'blue';
+
+if(Color == borka){
+    console.log(' color')
+}else{
+    console.log(`borka`)
+}
